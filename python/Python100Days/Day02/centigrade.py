8a4fde564ff64d8b0c4fe54947435f5471519838
ZhuDin <279388213@qq.com>
2022-03-22 00:02:23 +0800

    python 100 days exercise

---
+++ python/Python100Days/Day02/centigrade.py
@@ -0,0 +1,3 @@
+f = float(input('please enter centigrade: '))
+c = (f-32)/1.8
+print('%.1f c = %.1f degree' %(f,c))
---
+++ python/Python100Days/Day02/circle.py
@@ -0,0 +1,7 @@
+import math
+
+radius = float(input('please enter radius: '))
+perimeter = 2 * math.pi * radius
+area = math.pi * radius * radius
+print('perimeter: %.2f' % perimeter)
+print('area: %.2f' % area)
